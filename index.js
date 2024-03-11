5f008483279bcc89a9f60257e16acbe267699f81
AbinayaGnanasekaran <abinayagnanasekaran013@gmail.com>
2024-03-11 14:32:33 +0530

    Expense

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Expense Tracker
+ * 
+ * Adding a new expense -> /add-expemnse  =>post - expense details
+ * 
+ * displaying existing records -> /get-expenses  =>get
+ * 
+ * delete an expense -> /del-expem  =>post : id of the entry
+ * 
+ * update =>/update-exp  => post : id of the entry,expense details
+ * 
+ */
